c98afe6e73598e96c2cf063cffdc5dbe3c3b60da
steven-frasica <steven.frasica@gmail.com>
2026-04-25 09:31:26 -0400

    start video player page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import { Routes, Route } from 'react-router-dom';
+import Player from './pages/Player/Player';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/player/:id' element={<Player />} />
       </Routes>
     </div>
   )
