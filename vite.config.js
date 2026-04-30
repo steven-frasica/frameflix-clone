5c921c540e6dcfe9eed84f249f38ba1cf543e46c
steven-frasica <steven.frasica@gmail.com>
2026-04-30 11:26:50 -0400

    rename paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/netflix-clone/' : '/',
+  base: command === 'build' ? '/frameflix-clone/' : '/',
   build: {
     outDir: 'docs',
   },
